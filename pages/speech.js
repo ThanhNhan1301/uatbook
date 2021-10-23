b15360fd6298d33950c38cd32bdd3bff9a87aced
ThanhNhan1301 <huynh235@gmail.com>
2021-10-23 22:55:45 +0700

    add speech

--- pages/speech.js
+++ pages/speech.js
@@ -1,26 +1,34 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
 export default function Speech() {
-    const refRecog = useRef(null)
+    const [recognition, setRecognition] = useState()
     useEffect(() => {
-        const SpeechRecognition = SpeechRecognition || webkitSpeechRecognition
+        const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition
         const recognition = new SpeechRecognition()
         recognition.interResults = true
         recognition.lang = 'vi-VI'
         recognition.cotinuous = false
-        refRecog.current = recognition
+        setRecognition(recognition)
     }, [])
-
-    const recognition = refRecog.current
+    if (recognition) {
+        recognition.onspeechend = () => {
+            recognition.stop()
+        }
+        recognition.onerror = (err) => {
+            console.log(err)
+        }
+        recognition.onresult = (event) => {
+            console.log(event)
+        }
+    }
     return (
         <div className='mt-20 mx-auto text-center'>
             <button
-                onClick={() => recognition.start()}
+                onClick={() => recognition?.start()}
                 className='mr-5 py-2 px-4 bg-red-400 text-white'
             >
                 Listen
             </button>
-            <button className='py-2 px-4 bg-blue-400 text-white'>Stop Lister</button>
         </div>
     )
 }
